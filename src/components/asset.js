72a63bf46f21c839742a95b902975df02ac48c1d
Incraigulous <me@craigwann.com>
2019-02-15 14:34:52 -0600

    Adding background color to asset previews.

---
+++ src/components/asset.js
@@ -0,0 +1,46 @@
+export default {
+  template: ` <div>
+    <button class="cf-btn-secondary" @click="handleClick">Choose Asset</button> 
+    <div v-if="input">
+      <img :src="this.input" style="max-width: 320px; max-height: 320px; margin-top: 15px; background-color: #E5EBED;">
+      <button @click="close">
+        <icon icon="close"/>
+      </button>
+    </div>
+  </div>`,
+  props: {
+    value: String,
+    language: {
+      default() {
+        return ["en-US"];
+      }
+    }
+  },
+  computed: {
+    input: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$root.extensionsApi.dialogs
+        .selectSingleAsset()
+        .then(selectedAsset => {
+          if (selectedAsset) {
+            this.input =
+              "https:" + selectedAsset.fields.file[this.language].url;
+          } else {
+            this.input = null;
+          }
+        });
+    },
+    close() {
+      this.input = null;
+    }
+  }
+};
